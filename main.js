5ffe07cfad24667e162b993dd029f7744ae60682
amogha999 <119056318+amogha999@users.noreply.github.com>
2022-12-14 16:44:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,15 +7,15 @@ imagge.setAttribute("src",randomimage);
 var d=Math.random();
 var e=(d*6)+1;
 var f=Math.floor(e);
-var randomimage1=("dice"+e+".png");
+var randomimage1=("dice"+f+".png");
 var imaggge=document.querySelectorAll("img")[1];
 imaggge.setAttribute("src",randomimage1);
 if(b>e){
-    document.querySelector("h1").innerHTML("Player 1 wins");
+    document.querySelector("h1").innerHTML="Player 1 wins";
 }
 else if(e>b){
-    document.querySelector("h1").innerHTML("Player 2 wins");
+    document.querySelector("h1").innerHTML="Player 2 wins";
 }
 else{
-    document.querySelector("h1").innerHTML("Match has been Drawn");
+    document.querySelector("h1").innerHTML="Match has been Drawn";
 }
