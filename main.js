dcf4936877ad0a702657de7a383d0ff941614d60
amogha999 <119056318+amogha999@users.noreply.github.com>
2022-12-14 16:33:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var a=Math.random();
+var b= (a*6)+1;
+var c=Math.floor(b);
+var randomimage=("dice"+c+".png");
+var imagge=document.querySelectorAll("img")[0];
+imagge.setAttribute("src",randomimage);
+var d=Math.random();
+var e=(d*6)+1;
+var f=Math.floor(e);
+var randomimage1=("dice"+e+".png");
+var imaggge=document.querySelectorAll("img")[1];
+imaggge.setAttribute("src",randomimage1);
+if(b>e){
+    document.querySelector("h1").innerHTML("Player 1 wins");
+}
+else if(e>b){
+    document.querySelector("h1").innerHTML("Player 2 wins");
+}
+else{
+    document.querySelector("h1").innerHTML("Match has been Drawn");
+}
